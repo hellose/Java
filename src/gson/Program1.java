a209c6c11f067d48cac7050b1653adb1c5e8399d
sehyun <hyun.choi499@gmail.com>
2022-06-05 08:08:57 +0900

    add: gson test

---
+++ src/gson/Program1.java
@@ -0,0 +1,48 @@
+package gson;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+//추상 클래스 
+//JsonElement(js 문법 앱핑 Java클래스)
+
+//구현 클래스 
+//JsonPrimitive(js의 string,boolean,number,null)
+//JsonArray(js의 배열)
+//JsonObject(js의 객체)
+
+public class Program1 {
+	public static void main(String[] args) {
+
+		Gson gson = new Gson();
+
+		// 1. js객체 생성
+		JsonObject jsObject = new JsonObject();
+		// - public String toJson(JsonElement jsonElement)
+		System.out.println("빈 객체: " + gson.toJson(jsObject));
+
+		// 2. js객체에 js기본타입(string) 추가
+		// jsObject.add("property1", "스트링"); 아래는 좀더 복잡한 방법으로
+		JsonPrimitive primitiveString = new JsonPrimitive("스트링"); // String,Char,boolean등 사용가능
+		jsObject.add("primitive", primitiveString);
+		System.out.println("스트링 추가: " + gson.toJson(jsObject));
+
+		// 3. js객체에 js배열 추가
+		JsonArray jsArray = new JsonArray();
+		// js배열에 아이템 추가
+		jsArray.add(1); //// add(Number number)
+		jsArray.add(2);
+		jsArray.add(3);
+		jsObject.add("array", jsArray);
+		System.out.println("배열 추가: " + gson.toJson(jsObject));
+
+		// 4. js객체에 js객체 추가
+		JsonObject jsObject2 = new JsonObject();
+		jsObject2.addProperty("property", "string");
+		jsObject.add("object", jsObject2);
+		System.out.println("객체 추가: " + gson.toJson(jsObject));
+
+	}
+}
---
+++ src/gson/Program2.java
@@ -0,0 +1,32 @@
+package gson;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+
+		Person person = new Person("김", 10);
+
+		JsonElement el1 = gson.toJsonTree(person);
+		// {"name":"김","age":10}
+		System.out.println(el1);
+		System.out.println();
+
+		List<Person> persons = new ArrayList<>();
+		Person person2 = new Person("이", 20);
+		persons.add(person);
+		persons.add(person2);
+
+		JsonElement el2 = gson.toJsonTree(persons);
+		// [{"name":"김","age":10},{"name":"이","age":20}]
+		System.out.println(el2);
+		System.out.println();
+	}
+
+}
