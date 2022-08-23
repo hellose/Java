25dfd90e8d7e87206cf04da10103f1a4f20a4db0
hyun <hyun.choi499@gmail.com>
2022-08-23 13:22:28 +0900

    Multi Thread Singleton

---
+++ src/DesignPattern/SingletonPattern/LazyHolderSingleton.java
@@ -0,0 +1,16 @@
+package DesignPattern.SingletonPattern;
+
+class LazyHolderSingleton {
+
+	private LazyHolderSingleton() {
+	}
+
+	public static LazyHolderSingleton getInstance() {
+		return LazyHolder.INSTANCE;
+	}
+
+	private static class LazyHolder {
+		// 클래스 메모리 로딩시 INSTANCE 변수 초기화
+		private static final LazyHolderSingleton INSTANCE = new LazyHolderSingleton();
+	}
+}
---
+++ src/DesignPattern/SingletonPattern/MultiThreadSingleton.java
@@ -0,0 +1,19 @@
+package DesignPattern.SingletonPattern;
+
+class MultiThreadSingleton {
+
+	private static MultiThreadSingleton INSTANCE = null;
+
+	private MultiThreadSingleton() {
+
+	}
+
+	public static synchronized MultiThreadSingleton getInstance() {
+		if (INSTANCE == null) {
+			INSTANCE = new MultiThreadSingleton();
+		}
+
+		return INSTANCE;
+	}
+
+}
